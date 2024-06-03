8a94412627959ccb514fee430abda18f5afae309
Gurjot Saini <gurjotsaini1992@gmail.com>
2024-06-03 08:33:55 -0400

    Added: Product class

---
+++ Products/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Products
+{
+	public class Product
+	{
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncrementStock()
+        {
+            Stock += 1;
+        }
+
+        public void DecrementStock()
+        {
+            Stock -= 1;
+        }
+    }
+}
+
